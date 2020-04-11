889d1e49c5e3b538504c030b9278b5f9789efe5a
eraporsmk <eraporsmk@gmail.com>
2020-04-11 19:16:48 +0700

    update versi 5.0.8

--- app/Providers/HelperServiceProvider.php
+++ app/Providers/HelperServiceProvider.php
@@ -61,6 +61,7 @@ class HelperServiceProvider extends ServiceProvider
 		return $data[$query];
 	}
 	public static function get_ta(){
+		//add $check
 		$check = DB::select('select column_name from information_schema.columns where table_schema = ? and table_name = ?',['ref', 'semester']);
 		$user = auth()->user();
 		if($check){
