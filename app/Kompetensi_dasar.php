a8e347141f1463ab506da78a9cd89f052e9349fe
masadi <masadi.com@gmail.com>
2022-07-13 15:35:37 +0700

    edit

--- app/Kompetensi_dasar.php
+++ app/Kompetensi_dasar.php
@@ -14,9 +14,9 @@ class Kompetensi_dasar extends Model
 	protected $primaryKey = 'kompetensi_dasar_id';
 	protected $guarded = [];
 	public function mata_pelajaran(){
-		return $this->hasOne('App\Mata_pelajaran', 'mata_pelajaran_id', 'mata_pelajaran_id');
+		return $this->hasOne(Mata_pelajaran::class, 'mata_pelajaran_id', 'mata_pelajaran_id');
 	}
 	public function pembelajaran(){
-		return $this->hasOne('App\Pembelajaran', 'mata_pelajaran_id', 'mata_pelajaran_id');
+		return $this->hasOne(Pembelajaran::class, 'mata_pelajaran_id', 'mata_pelajaran_id');
 	}
 }
